7dec5e6226203e0c38118cb566521a4f55166434
kebbartley <keb.bartley@gmail.com>
2021-12-02 16:03:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+lines = open("input2.txt", "r")
+x = 0
+z = 0
+aim = 0
+for line in lines:
+  parts = line.split(' ')
+  direction = parts[0]
+  val = int(parts[1])
+  if (direction == 'forward'):
+    x += val
+    z += (val * aim) 
+  elif (direction == 'down'):
+    aim += val
+  elif (direction == 'up'):
+    aim -= val
+print(z * x)
+
+  
+
